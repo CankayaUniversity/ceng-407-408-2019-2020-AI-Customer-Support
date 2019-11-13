d911bbcc73f86256fefabb03c455ee0dcb3faa6c
atakde <mehata1997@hotmail.com>
2019-11-13 21:07:02 +0300

    Index :: initial commit for the project

---
+++ index.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+    <!-- META TAGS -->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Knowledge Base Template</title>
+
+    <link rel="shortcut icon" href="images/favicon.png" />
+
+    <!-- Fonts-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+
+    <!-- Style Sheet-->
+    <link rel='stylesheet' id='bootstrap-css-css' href='css/bootstrap.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='responsive-css-css' href='css/res.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='main-css-css' href='css/site.css' type='text/css' media='all' />
+
+</head>
+
+<body>
+    <div class="search-area-wrapper">
+        <div class="search-area container">
+            <h3 class="search-header">Have a Question?</h3>
+            <p class="search-tag-line">If you have any question you can ask below or enter what you are looking for!</p>
+
+            <form id="search-form" class="search-form clearfix" method="get" action="#" autocomplete="off">
+                <input class="search-term required" type="text" id="s" name="s" placeholder="Type your search terms here" title="* Please enter a search term!" />
+                <input class="search-btn" type="submit" value="Search" />
+                <div id="search-error-container"></div>
+            </form>
+        </div>
+    </div>
+    <div class="page-container">
+        <div class="container">
+            <div class="row">
+                <div class="span8 page-content">
+                    <div class="row separator">
+                        <section class="span8 articles-list">
+                            <h3>Featured Articles</h3>
+                            <ul class="articles">
+                                <li class="article-entry standard">
+                                    <h4><a href="single.html">Integrating WordPress with Your Website</a></h4>
+                                    <span class="article-meta">25 Feb, 2013 in <a href="#" title="View all posts in Server &amp; Database">Server &amp; Database</a></span>
+                                    <span class="like-count">66</span>
+                                </li>
+                                <li class="article-entry standard">
+                                    <h4><a href="single.html">WordPress Site Maintenance</a></h4>
+                                    <span class="article-meta">24 Feb, 2013 in <a href="#" title="View all posts in Website Dev">Website Dev</a></span>
+                                    <span class="like-count">15</span>
+                                </li>
+                                <li class="article-entry video">
+                                    <h4><a href="single.html">Meta Tags in WordPress</a></h4>
+                                    <span class="article-meta">23 Feb, 2013 in <a href="#" title="View all posts in Website Dev">Website Dev</a></span>
+                                    <span class="like-count">8</span>
+                                </li>
+                                <li class="article-entry image">
+                                    <h4><a href="single.html">WordPress in Your Language</a></h4>
+                                    <span class="article-meta">22 Feb, 2013 in <a href="#" title="View all posts in Advanced Techniques">Advanced Techniques</a></span>
+                                    <span class="like-count">6</span>
+                                </li>
+                                <li class="article-entry standard">
+                                    <h4><a href="single.html">Know Your Sources</a></h4>
+                                    <span class="article-meta">22 Feb, 2013 in <a href="#" title="View all posts in Website Dev">Website Dev</a></span>
+                                    <span class="like-count">2</span>
+                                </li>
+                                <li class="article-entry standard">
+                                    <h4><a href="single.html">Validating a Website</a></h4>
+                                    <span class="article-meta">21 Feb, 2013 in <a href="#" title="View all posts in Website Dev">Website Dev</a></span>
+                                    <span class="like-count">3</span>
+                                </li>
+                                <li class="article-entry standard">
+                                    <h4><a href="single.html">Validating a Website</a></h4>
+                                    <span class="article-meta">21 Feb, 2013 in <a href="#" title="View all posts in Website Dev">Website Dev</a></span>
+                                    <span class="like-count">3</span>
+                                </li>
+                                <li class="article-entry standard">
+                                    <h4><a href="single.html">Validating a Website</a></h4>
+                                    <span class="article-meta">21 Feb, 2013 in <a href="#" title="View all posts in Website Dev">Website Dev</a></span>
+                                    <span class="like-count">3</span>
+                                </li>
+                                <li class="article-entry standard">
+                                    <h4><a href="single.html">Validating a Website</a></h4>
+                                    <span class="article-meta">21 Feb, 2013 in <a href="#" title="View all posts in Website Dev">Website Dev</a></span>
+                                    <span class="like-count">3</span>
+                                </li>
+                            </ul>
+                        </section>
+                    </div>
+                </div>
+                <aside class="span4 page-sidebar">
+
+                <button type="button" class="btn btn-info btn-lg btn-block">Ask a Question</button>
+                </p>
+
+                    <section class="widget">
+                        <div class="support-widget">
+                            <h3 class="title">Support</h3>
+                            <p class="intro">Need more support? If you did not found an answer, contact us for further help.</p>
+                        </div>
+                    </section>
+
+                    <section class="widget">
+                        <div class="quick-links-widget">
+                            <h3 class="title">Quick Links</h3>
+                            <ul id="menu-quick-links" class="menu clearfix">
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="articles-list.html">Articles List</a></li>
+                                <li><a href="faq.html">FAQs</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                    </section>
+                    </p>
+                    <section class="widget">
+                        <div class="tags-widget">
+                        <h3 class="title">Tags</h3>
+                        <div class="tagcloud">
+                            <a href="#" class="btn btn-mini">basic</a>
+                            <a href="#" class="btn btn-mini">beginner</a>
+                            <a href="#" class="btn btn-mini">blogging</a>
+                            <a href="#" class="btn btn-mini">colour</a>
+                            <a href="#" class="btn btn-mini">css</a>
+                            <a href="#" class="btn btn-mini">date</a>
+                            <a href="#" class="btn btn-mini">design</a>
+                            <a href="#" class="btn btn-mini">files</a>
+                            <a href="#" class="btn btn-mini">format</a>
+                            <a href="#" class="btn btn-mini">header</a>
+                            <a href="#" class="btn btn-mini">images</a>
+                            <a href="#" class="btn btn-mini">plugins</a>
+                            <a href="#" class="btn btn-mini">setting</a>
+                            <a href="#" class="btn btn-mini">templates</a>
+                            <a href="#" class="btn btn-mini">theme</a>
+                            <a href="#" class="btn btn-mini">time</a>
+                            <a href="#" class="btn btn-mini">videos</a>
+                            <a href="#" class="btn btn-mini">website</a>
+                            <a href="#" class="btn btn-mini">wordpress</a>
+                        </div></div>
+                    </section>
+
+                </aside>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
