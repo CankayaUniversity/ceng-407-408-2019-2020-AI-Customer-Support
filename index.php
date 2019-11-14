9cae9dcbf25c5c159334819efd7768e68c961f7a
atakde <mehata1997@hotmail.com>
2019-11-14 19:46:06 +0300

    Index :: fixed right boxed, added new styles

--- index.php
+++ index.php
@@ -127,25 +127,10 @@
                         <div class="tags-widget">
                         <h3 class="title">Tags</h3>
                         <div class="tagcloud">
-                            <a href="#" class="btn btn-mini">basic</a>
-                            <a href="#" class="btn btn-mini">beginner</a>
-                            <a href="#" class="btn btn-mini">blogging</a>
-                            <a href="#" class="btn btn-mini">colour</a>
-                            <a href="#" class="btn btn-mini">css</a>
-                            <a href="#" class="btn btn-mini">date</a>
-                            <a href="#" class="btn btn-mini">design</a>
-                            <a href="#" class="btn btn-mini">files</a>
-                            <a href="#" class="btn btn-mini">format</a>
-                            <a href="#" class="btn btn-mini">header</a>
-                            <a href="#" class="btn btn-mini">images</a>
-                            <a href="#" class="btn btn-mini">plugins</a>
-                            <a href="#" class="btn btn-mini">setting</a>
-                            <a href="#" class="btn btn-mini">templates</a>
-                            <a href="#" class="btn btn-mini">theme</a>
-                            <a href="#" class="btn btn-mini">time</a>
-                            <a href="#" class="btn btn-mini">videos</a>
-                            <a href="#" class="btn btn-mini">website</a>
-                            <a href="#" class="btn btn-mini">wordpress</a>
+                            <a href="#" class="badge badge-pill badge-warning">basic</a>
+                            <a href="#" class="badge badge-pill badge-warning">beginner</a>
+                            <a href="#" class="badge badge-pill badge-warning">blogging</a>
+                            <a href="#" class="badge badge-pill badge-warning">colour</a>
                         </div></div>
                     </section>
 
