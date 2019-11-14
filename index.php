012b30a9ff1aa026a3b33e4edf40e5c63210d24f
atakde <mehata1997@hotmail.com>
2019-11-15 01:00:24 +0300

    Index :: changed style, updated css

--- index.php
+++ index.php
@@ -73,7 +73,7 @@
                 <div class="span8 page-content">
                     <div class="row separator">
                         <section class="span8 articles-list">
-                            <div class="my-3 p-3 bg-white rounded box-shadow">
+                            <div class="my-3 p-3 rounded box-shadow">
                                 <h3 class="border-bottom border-gray pb-2 mb-0">Recent updates</h3>
                                 <div class="media text-muted pt-3">
                                     <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
