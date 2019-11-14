c0768b8673b8243b2976d2330b6c271e43aee307
atakde <mehata1997@hotmail.com>
2019-11-14 19:54:30 +0300

    Index :: formatted file

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en-US">
+
 <head>
     <!-- META TAGS -->
     <meta charset="UTF-8" />
@@ -29,15 +30,15 @@
             <p class="search-tag-line">If you have any question you can ask below or enter what you are looking for!</p>
 
             <form id="search-form" class="search-form clearfix" method="get" action="#" autocomplete="off">
-            <div class="input-group">
-    <input type="text" class="form-control" placeholder="Search this blog">
-    <div class="input-group-append">
-      <button class="btn btn-secondary" type="button">
-        <i class="fa fa-search"></i>
-      </button>
-    </div>
-  </div>
-  
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search this blog">
+                    <div class="input-group-append">
+                        <button class="btn btn-secondary" type="button">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                </div>
+
                 <div id="search-error-container"></div>
             </form>
         </div>
@@ -48,40 +49,37 @@
                 <div class="span8 page-content">
                     <div class="row separator">
                         <section class="span8 articles-list">
-        <div class="my-3 p-3 bg-white rounded box-shadow">
-        <h3 class="border-bottom border-gray pb-2 mb-0">Recent updates</h3>
-        <div class="media text-muted pt-3">
-          <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
-          <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-            <strong class="d-block text-gray-dark">@username</strong>
-            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-          </p>
-        </div>
-        <div class="media text-muted pt-3">
-          <img data-src="holder.js/32x32?theme=thumb&bg=e83e8c&fg=e83e8c&size=1" alt="" class="mr-2 rounded">
-          <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-            <strong class="d-block text-gray-dark">@username</strong>
-            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-          </p>
-        </div>
-        <div class="media text-muted pt-3">
-          <img data-src="holder.js/32x32?theme=thumb&bg=6f42c1&fg=6f42c1&size=1" alt="" class="mr-2 rounded">
-          <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-            <strong class="d-block text-gray-dark">@username</strong>
-            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-          </p>
-        </div>
-        <small class="d-block text-right mt-3">
+                            <div class="my-3 p-3 bg-white rounded box-shadow">
+                                <h3 class="border-bottom border-gray pb-2 mb-0">Recent updates</h3>
+                                <div class="media text-muted pt-3">
+                                    <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
+                                    <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                                        <strong class="d-block text-gray-dark">@username</strong> Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                                    </p>
+                                </div>
+                                <div class="media text-muted pt-3">
+                                    <img data-src="holder.js/32x32?theme=thumb&bg=e83e8c&fg=e83e8c&size=1" alt="" class="mr-2 rounded">
+                                    <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                                        <strong class="d-block text-gray-dark">@username</strong> Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                                    </p>
+                                </div>
+                                <div class="media text-muted pt-3">
+                                    <img data-src="holder.js/32x32?theme=thumb&bg=6f42c1&fg=6f42c1&size=1" alt="" class="mr-2 rounded">
+                                    <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                                        <strong class="d-block text-gray-dark">@username</strong> Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                                    </p>
+                                </div>
+                                <small class="d-block text-right mt-3">
           <a href="#">All updates</a>
         </small>
-      </div>
+                            </div>
                         </section>
                     </div>
                 </div>
                 <aside class="span3 page-sidebar">
 
-                <button type="button" class="btn btn-info btn-lg btn-block">Ask a Question</button>
-                </p>
+                    <button type="button" class="btn btn-info btn-lg btn-block">Ask a Question</button>
+                    </p>
 
                     <section class="widget">
                         <div class="support-widget">
@@ -104,13 +102,14 @@
                     </p>
                     <section class="widget">
                         <div class="tags-widget">
-                        <h3 class="title">Tags</h3>
-                        <div class="tagcloud">
-                            <a href="#" class="badge badge-pill badge-warning">basic</a>
-                            <a href="#" class="badge badge-pill badge-warning">beginner</a>
-                            <a href="#" class="badge badge-pill badge-warning">blogging</a>
-                            <a href="#" class="badge badge-pill badge-warning">colour</a>
-                        </div></div>
+                            <h3 class="title">Tags</h3>
+                            <div class="tagcloud">
+                                <a href="#" class="badge badge-pill badge-warning">basic</a>
+                                <a href="#" class="badge badge-pill badge-warning">beginner</a>
+                                <a href="#" class="badge badge-pill badge-warning">blogging</a>
+                                <a href="#" class="badge badge-pill badge-warning">colour</a>
+                            </div>
+                        </div>
                     </section>
 
                 </aside>
