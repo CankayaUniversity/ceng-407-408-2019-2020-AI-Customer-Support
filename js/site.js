8da6537b90485ad7e640aad0f792ef85553f0144
Arınç Alp Eren <arinc.alp.98@gmail.com>
2019-11-17 23:32:18 +0300

    header.php :: Updated nav element

---
+++ js/site.js
@@ -0,0 +1,5 @@
+window.onscroll = () => {
+    const nav = document.querySelector('#navbar');
+    if(this.scrollY <= 250) nav.className = 'navbar navbar-expand-lg navbar-dark navbar-custom fixed-top'; 
+    else nav.className = 'scroll navbar navbar-expand-lg navbar-dark navbar-custom fixed-top';
+  };
