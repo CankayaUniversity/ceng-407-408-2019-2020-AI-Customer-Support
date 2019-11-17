571aa10aa07a00669d406e199661462c7b4b3669
atakde <mehata1997@hotmail.com>
2019-11-17 20:10:25 +0300

    Header :: transport navbar to header.php

--- index.php
+++ index.php
@@ -2,29 +2,6 @@
 
 <body>
 
-<!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="#">AI Customer Support</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Sign Up</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Log In</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Profile</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
     <div class="search-area-wrapper">
         <div class="search-area container">
             <h3 class="search-header">Have a Question?</h3>
