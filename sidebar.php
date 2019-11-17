c202b6c71a8fa534dc8ed702e6dbcc84a658591e
atakde <mehata1997@hotmail.com>
2019-11-17 20:51:03 +0300

    Sidebar :: parsed html for sidebar

---
+++ sidebar.php
@@ -0,0 +1,37 @@
+<aside class="span3 page-sidebar">
+
+<button type="button" class="btn btn-info btn-lg btn-block">Ask a Question</button>
+</p>
+
+<section class="widget">
+    <div class="support-widget">
+        <h3 class="title">Support</h3>
+        <p class="intro">Need more support? If you did not found an answer, contact us for further help.</p>
+    </div>
+</section>
+
+<section class="widget">
+    <div class="quick-links-widget">
+        <h3 class="title">Quick Links</h3>
+        <ul id="menu-quick-links" class="menu clearfix">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="articles-list.html">Articles List</a></li>
+            <li><a href="faq.html">FAQs</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </div>
+</section>
+</p>
+<section class="widget">
+    <div class="tags-widget">
+        <h3 class="title">Tags</h3>
+        <div class="tagcloud">
+            <a href="#" class="badge badge-pill badge-warning">basic</a>
+            <a href="#" class="badge badge-pill badge-warning">beginner</a>
+            <a href="#" class="badge badge-pill badge-warning">blogging</a>
+            <a href="#" class="badge badge-pill badge-warning">colour</a>
+        </div>
+    </div>
+</section>
+
+</aside>
