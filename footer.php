dcf2a026b18f94d7c7fdc80c8254cf9a6ba2643f
atakde <mehata1997@hotmail.com>
2019-11-17 18:11:03 +0300

    Index :: parsed html and created master page structure

---
+++ footer.php
@@ -0,0 +1,14 @@
+<div class="footer-copyright">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <p>Copyright AI Customer Support © 2019. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</div>
+</footer>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,27 +1,4 @@
-<!doctype html>
-<html lang="en-US">
-
-<head>
-    <!-- META TAGS -->
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>AI Customer Support</title>
-
-    <link rel="shortcut icon" href="images/favicon.png" />
-
-    <!-- Fonts-->
-    <script defer src="https://use.fontawesome.com/releases/v5.2.0/js/all.js" integrity="sha384-4oV5EgaV02iISL2ban6c/RmotsABqE4yZxZLcYMAdG7FAPsyHYAPpywE9PJo+Khy" crossorigin="anonymous"></script>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-
-    <!-- Style Sheet-->
-    <link rel='stylesheet' id='bootstrap-css-css' href='css/bootstrap.css' type='text/css' media='all' />
-    <link rel='stylesheet' id='responsive-css-css' href='css/res.css' type='text/css' media='all' />
-    <link rel='stylesheet' id='main-css-css' href='css/site.css' type='text/css' media='all' />
-
-</head>
+<?php include "header.php"; ?>
 
 <body>
 
@@ -190,17 +167,4 @@
     </div>
 </div>
   
-<div class="footer-copyright">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <p>Copyright AI Customer Support © 2019. All rights reserved.</p>
-            </div>
-        </div>
-    </div>
-</div>
-</footer>
-
-</body>
-
-</html>
+<?php include "footer.php"; ?>
