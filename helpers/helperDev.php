d5b32621d79c2f71ec2dcb3e8e28a515c22bb33f
atakde <mehata1997@hotmail.com>
2019-11-22 21:52:50 +0300

    helperDev :: added exit

--- helpers/helperDev.php
+++ helpers/helperDev.php
@@ -5,4 +5,5 @@ if (file_exists($file_pointer)) {
    // echo "The file $file_pointer exists!";
 } else {
     echo "The file $file_pointer does not exists, please contact with atakde :)";
+    exit(0);
 }
