138f2948c27e840de1caa408d4e3412e9265e8e9
atakde <mehata1997@hotmail.com>
2019-11-23 19:01:29 +0300

    helperDev :: checking the environment for developers

--- helpers/helperDev.php
+++ helpers/helperDev.php
@@ -1,9 +1,20 @@
 <?php
+
+/* *** Check config file *** */
+
 $file_pointer = 'inc/config.php';
 
 if (file_exists($file_pointer)) {
-   // echo "The file $file_pointer exists!";
+    // echo "The file $file_pointer exists!";
 } else {
     echo "The file $file_pointer does not exists, please contact with atakde :)";
     exit(0);
+}
+
+/* *** Check environment *** */
+
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $environment = 'dev';
+} else {
+    $environment = 'live';
 }
