f3ff8582e8f71d3588122276c72af4128a0da7f6
atakde <mehata1997@hotmail.com>
2019-11-27 00:40:41 +0300

    Index :: dynamically content added

--- index.php
+++ index.php
@@ -29,24 +29,18 @@
                         <section class="span8 articles-list">
                             <div class="my-3 p-3 rounded box-shadow">
                                 <h3 class="border-bottom border-gray pb-2 mb-0">Recent updates</h3>
+                                <?php
+                                $query = $conn->query("SELECT * FROM questions",PDO::FETCH_ASSOC);
+                                $getQuestions = $query->fetchAll(PDO::FETCH_ASSOC);
+                                ?>
+                                <?php foreach($getQuestions as $getQuestion) { ?>
                                 <div class="media text-muted pt-3">
                                     <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
                                     <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                                        <strong class="d-block text-gray-dark">@username</strong> Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                                        <strong class="d-block text-gray-dark"><? echo $getQuestion['q_author'] ?></strong> <? echo $getQuestion['q_title'] ?>
                                     </p>
                                 </div>
-                                <div class="media text-muted pt-3">
-                                    <img data-src="holder.js/32x32?theme=thumb&bg=e83e8c&fg=e83e8c&size=1" alt="" class="mr-2 rounded">
-                                    <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                                        <strong class="d-block text-gray-dark">@username</strong> Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-                                    </p>
-                                </div>
-                                <div class="media text-muted pt-3">
-                                    <img data-src="holder.js/32x32?theme=thumb&bg=6f42c1&fg=6f42c1&size=1" alt="" class="mr-2 rounded">
-                                    <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                                        <strong class="d-block text-gray-dark">@username</strong> Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-                                    </p>
-                                </div>
+                                <?php } ?>
                                 <small class="d-block text-right mt-3">
                                     <a href="#">All updates</a>
                                 </small>
