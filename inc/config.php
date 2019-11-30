dfd64d3d9b3e36e48a3512c3ac9b1b7df376f8ed
atakde <mehata1997@hotmail.com>
2019-11-30 13:05:01 +0300

    Config :: added temporary config

---
+++ inc/config.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "12345678";
+$dbname = "customer_support";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
