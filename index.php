02501963306812ea09f9fee9366042397420febe
atakde <mehata1997@hotmail.com>
2019-12-01 12:46:37 +0300

    Index :: added pagination homepage

--- index.php
+++ index.php
@@ -33,30 +33,48 @@
             <h4 class="font-weight-bold mt-0 mb-4">Recent updates</h4>
             <div class="bg-white card mb-4 order-list shadow-sm">
             <?php
-                $query = $conn->query("SELECT * FROM questions",PDO::FETCH_ASSOC);
-                $getQuestions = $query->fetchAll(PDO::FETCH_ASSOC);
+                $limit = 5;
+                $query = "SELECT * FROM questions";
+                $s = $conn->prepare($query);
+                $s->execute();
+                $total_results = $s->rowCount();
+                $total_pages = ceil($total_results/$limit);
+                if (!isset($_GET['page'])) {
+                    $page = 1;
+                } else{
+                    $page = $_GET['page'];
+                }
+                $starting_limit = ($page-1)*$limit;
+                $show = "SELECT * FROM questions ORDER BY q_id DESC LIMIT $starting_limit, $limit";
+                $r = $conn->prepare($show);
+                $r->execute();
             ?>
-            <?php foreach($getQuestions as $getQuestion) { 
-                $q_author = $getQuestion['q_author'];
-                $user = $conn->query("SELECT user_id, firstname, surname, q_author FROM users,questions WHERE user_id='$q_author'",PDO::FETCH_ASSOC)->fetch();
-            ?>
+            <?php while($res = $r->fetch(PDO::FETCH_ASSOC)) :
+            $q_author = $res['q_author'];
+            $user = $conn->query("SELECT user_id, firstname, surname, q_author FROM users,questions WHERE user_id='$q_author'",PDO::FETCH_ASSOC)->fetch();?>
                 <div class="gold-members p-4">
                     <a href="#"> </a>
                     <div class="media">
                         <a href="#"> <img class="mr-4" src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="Generic placeholder image"> </a>
                         <div class="media-body">
-                            <a href="#"> <span class="float-right text-info">Delivered on <?php echo $getQuestion['q_date'] ?> <i class="icofont-check-circled text-success"></i></span> </a>
-                            <h6 class="mb-2"> <a href="#"></a> <a href="#" class="text-black"><?php echo $getQuestion['q_title'] ?></a></h6>
-                            <p class="text-gray mb-1"><i class="icofont-location-arrow"></i> <?php echo $getQuestion['q_description'] ?></p>
+                            <a href="#"> <span class="float-right text-info">Delivered on <?php echo $res['q_date'] ?> <i class="icofont-check-circled text-success"></i></span> </a>
+                            <h6 class="mb-2"> <a href="#"></a> <a href="#" class="text-black"><?php echo $res['q_title'] ?></a></h6>
+                            <p class="text-gray mb-1"><i class="icofont-location-arrow"></i> <?php echo $res['q_description'] ?></p>
                             <hr>
                             <div class="float-right"> <a class="btn btn-sm btn-outline-primary" href="#"><i class="icofont-headphone-alt"></i> HELP</a> <a class="btn btn-sm btn-primary" href="#"><i class="icofont-refresh"></i> REORDER</a></div>
                             <p class="mb-0 text-black text-primary pt-2"><span class="text-black font-weight-bold"> By </span> <?php echo $user['firstname'].' '.$user['surname'] ?></p>
                         </div>
                     </div>
                 </div>
-            <?php } ?>
+            <?php endwhile; ?>
             </div>
         </div>
+      <?php  for ($page=1; $page <= $total_pages ; $page++):?>
+
+<a href='<?php echo "?page=$page"; ?>' class="links"><?php echo $page; ?>
+</a>
+
+<?php endfor; ?>
     </div>
 </div>
                         </section>
