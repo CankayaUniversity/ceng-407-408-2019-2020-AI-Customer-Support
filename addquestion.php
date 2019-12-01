6d73db1cc9e6cfbd2e7bea44b16c60ffd2b660c5
atakde <mehata1997@hotmail.com>
2019-12-01 12:26:16 +0300

    addQuestion :: fix session and php tag

--- addquestion.php
+++ addquestion.php
@@ -2,7 +2,7 @@
 include 'header.php';
 include 'inc/config.php';
 session_start();
-if($_SESSION["user_Surname"] == null){
+if($sUsername == null){
     echo"<script>
     window.location.replace('index.php')
     </script>";
@@ -99,7 +99,7 @@ if (isset($_POST['QuestionSubmit']) && $qTitle != '' && $qDescription != ''){
     $qDescription = $_POST['QuestionDesc'];
     $qAuthor = $_SESSION["user_UserID"];
     $conn->exec("INSERT INTO questions(q_title,q_description,q_author) VALUES ('$qTitle','$qDescription','$qAuthor');");
-    ?><script>window.location.replace("index.php");</script><?
+    ?><script>window.location.replace("index.php");</script><?php
 }
 
 ?>
