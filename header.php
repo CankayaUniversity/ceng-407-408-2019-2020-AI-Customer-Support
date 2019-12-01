9dec70baf275970c51ec9ef0b747c116f372e31d
atakde <mehata1997@hotmail.com>
2019-12-01 11:47:06 +0300

    Header :: disable debugging

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
-ini_set('display_errors',1);
-error_reporting(-1);
+/* ini_set('display_errors',1);
+error_reporting(-1); */
 include 'helpers/helperMeta.php';
 include 'helpers/helperDev.php';
 include 'inc/config.php';
