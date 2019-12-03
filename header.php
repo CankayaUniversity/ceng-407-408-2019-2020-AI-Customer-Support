b0c973fa31fe0f5593df2df2caebe9693da50594
Arınç Alp Eren <40849529+alpekin98@users.noreply.github.com>
2019-12-03 14:42:24 +0200

    header.php :: Notification panel bug fixes

--- header.php
+++ header.php
@@ -262,44 +262,43 @@ $(document).ready(function(){
 
 $(document).ready(function () {
 
-// ANIMATEDLY DISPLAY THE NOTIFICATION COUNTER.
-$('#noti_Counter')
-    .css({ opacity: 0 })
-    .text('7')  // ADD DYNAMIC VALUE (YOU CAN EXTRACT DATA FROM DATABASE OR XML).
-    .css({ top: '-10px' })
-    .animate({ top: '-2px', opacity: 1 }, 500);
+  // ANIMATEDLY DISPLAY THE NOTIFICATION COUNTER.
+  $('#noti_Counter')
+      .css({ opacity: 0 })
+      .text('7')  // ADD DYNAMIC VALUE (YOU CAN EXTRACT DATA FROM DATABASE OR XML).
+      .css({ top: '-10px' })
+      .animate({ top: '-2px', opacity: 1 }, 500);
 
-$('#noti_Button').click(function () {
+  $('#noti_Button').click(function () {
 
-    // TOGGLE (SHOW OR HIDE) NOTIFICATION WINDOW.
-    $('#notifications').fadeToggle('fast', 'linear', function () {
-        if ($('#notifications').is(':hidden')) {
-            $('#noti_Button').css('background-color', '#2E467C');
-        }
-        // CHANGE BACKGROUND COLOR OF THE BUTTON.
-        else $('#noti_Button').css('background-color', '#FFF');
-    });
+      // TOGGLE (SHOW OR HIDE) NOTIFICATION WINDOW.
+      $('#notifications').fadeToggle('fast', 'linear', function () {
+          if ($('#notifications').is(':hidden')) {
+              $('#noti_Button').css('background-color', '#49afcd');
+          }
+          // CHANGE BACKGROUND COLOR OF THE BUTTON.
+          else $('#noti_Button').css('background-color', '#49afcd');
+      });
 
-    $('#noti_Counter').fadeOut('slow');     // HIDE THE COUNTER.
+      $('#noti_Counter').fadeOut('slow');     // HIDE THE COUNTER.
 
-    return false;
+      return false;
+  });
+
+  // HIDE NOTIFICATIONS WHEN CLICKED ANYWHERE ON THE PAGE.
+  $(document).click(function () {
+      $('#notifications').hide();
+
+      // CHECK IF NOTIFICATION COUNTER IS HIDDEN.
+      if ($('#noti_Counter').is(':hidden')) {
+          // CHANGE BACKGROUND COLOR OF THE BUTTON.
+          $('#noti_Button').css('background-color', '#49afcd');
+      }
+  });
+
+  $('#notifications').click(function () {
+      return false;       // DO NOTHING WHEN CONTAINER IS CLICKED.
+  });
 });
-
-// HIDE NOTIFICATIONS WHEN CLICKED ANYWHERE ON THE PAGE.
-$(document).click(function () {
-    $('#notifications').hide();
-
-    // CHECK IF NOTIFICATION COUNTER IS HIDDEN.
-    if ($('#noti_Counter').is(':hidden')) {
-        // CHANGE BACKGROUND COLOR OF THE BUTTON.
-        $('#noti_Button').css('background-color', '#2E467C');
-    }
-});
-
-$('#notifications').click(function () {
-    return false;       // DO NOTHING WHEN CONTAINER IS CLICKED.
-});
-});
-
 
 </script>
