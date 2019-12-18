2e29abfd486de77979cf3aecfa4d32bd66dbb150
atakde <mehata1997@hotmail.com>
2019-12-18 19:53:40 +0300

    Index :: fix version

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">Admin Dashboard <sup>1.0</sup></div>
       </a>
 
       <!-- Divider -->
