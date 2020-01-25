6018177fd2752c26b4477c04d3aab9b210ec95e9
atakde <mehata1997@hotmail.com>
2020-01-25 17:20:05 +0300

    Header :: update css and js file path

--- header.php
+++ header.php
@@ -25,7 +25,6 @@ if (isset($_SESSION['user_Username'])) {
 <!doctype html>
 <html lang="en-US">
 <head>
-<base href="/" />
     <!-- META TAGS -->
 
     <?php if (preg_match('/single|category|userpage|profile/', $_SERVER['REQUEST_URI'])) {
@@ -95,12 +94,12 @@ if (isset($_SESSION['user_Username'])) {
 
     <!-- Style Sheet-->
     <script src="js/jquery.min.js"></script>
-    <link rel='stylesheet' id='responsive-css-css' href='css/res.css' type='text/css' media='all' />
-    <link rel='stylesheet' id='main-css-css' href='css/site.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='responsive-css-css' href='/css/res.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='main-css-css' href='/css/site.css' type='text/css' media='all' />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-    <script type="text/javascript" src="js/site.js"></script>
+    <script type="text/javascript" src="/js/site.js"></script>
 
 </head>
 
