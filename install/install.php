befe445baedd19d7a72faa04d481eb29deb21d39
atakde <mehata1997@hotmail.com>
2020-01-26 19:05:34 +0300

    Install :: fix console errors

--- install/install.php
+++ install/install.php
@@ -1,9 +1,9 @@
 <head>
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Install Customer Support DB</title>
-    <link rel="shortcut icon" href="images/favicon.png" />
+    <link rel="shortcut icon" href="/images/favicon.ico" />
     <!-- Style Sheet-->
-    <script src="js/jquery.min.js"></script>
+    <script src="/js/jquery.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">  
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </head>
