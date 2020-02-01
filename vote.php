0ba69acc59e2741e8f3d47639b06d0a5367e0ff8
atakde <mehata1997@hotmail.com>
2020-02-01 17:00:30 +0300

    Like & Dislike :: User only vote one question once

--- vote.php
+++ vote.php
@@ -10,14 +10,27 @@ if($_SESSION["user_UserID"] == NULL){
     echo  "You must be logged in to vote.";
     return;
 }
-
+$user_id = $_SESSION['user_UserID'];
 $action = $_POST["action"];
 $q_id = $_POST['q_id'];
-
+$checkLikeData = $conn->query("SELECT status FROM like_data WHERE user_id = ".$user_id." AND q_id =".$q_id."", PDO::FETCH_ASSOC)->fetch();
 if($action == "like"){
-    $query = $conn->query("UPDATE questions SET q_like=q_like+1 WHERE q_id=$q_id"); 
+    if(empty($checkLikeData)) {
+        $query = $conn->query("UPDATE questions SET q_like=q_like+1 WHERE q_id=$q_id");
+        $logLikeData = "INSERT INTO like_data(q_id,user_id,status) VALUES ('$q_id', '$user_id', '1')";
+        $conn->exec($logLikeData);
+    }else {
+        echo "This cannot be done! You can only vote one question once!";
+    }
 }
 else if($action == "dislike"){
-    $query = $conn->query("UPDATE questions SET q_dislike=q_dislike+1 WHERE q_id=$q_id"); 
+    if(empty($checkLikeData)) {
+        $query = $conn->query("UPDATE questions SET q_dislike=q_dislike+1 WHERE q_id=$q_id");
+        $logLikeData = "INSERT INTO like_data(q_id,user_id,status) VALUES ('$q_id', '$user_id', '1')";
+        $conn->exec($logLikeData);
+    } else {
+        echo "This cannot be done! You can only vote one question once!";
+    }
+   
 }
 ?>
