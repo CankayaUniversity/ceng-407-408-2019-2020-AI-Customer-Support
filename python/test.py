dc5252b78cadea248e3430bb25ce0f01565fcf91
atakde <mehata1997@hotmail.com>
2020-02-02 14:59:25 +0300

    Python :: read dataset from json file

--- python/test.py
+++ python/test.py
@@ -1,7 +1,13 @@
 import sys
-
+import json
 print (sys.argv[1], sys.argv[2], sys.argv[3]), # get parameters
 
 print("Test")
+
+# read file
+myfile = []
+with open('python/qa_Electronics.json', 'r') as myfile:
+    data=myfile.read()
+
 
 
