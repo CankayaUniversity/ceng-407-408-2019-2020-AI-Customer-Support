4c12069616afd380f17a8fdf34047dd2f9b0ad2b
Arınç Alp Eren <40849529+alpekin98@users.noreply.github.com>
2020-02-22 15:21:49 +0300

    category :: new design

--- category.php
+++ category.php
@@ -21,9 +21,9 @@
                                     $q_title = $r["q_title"];
                                     $q_description = $r["q_description"];
                                     $q_author_id = $r['q_author'];
-                                    $q_date = $r['q_date'];
                                     $q_tag = $r['q_tags'];
                                     $q_like = $r['q_like'];
+                                    $q_dislike = $r['q_dislike'];
                                     $origin_q_date = $r['q_date'];
                                     $q_slug = $r['slug'];
                                     $newDate = date("d m Y", strtotime($origin_q_date));
@@ -31,13 +31,23 @@
                                     $user_id = $user['user_id'];
                                     $username = $user['username'];
                                 ?>
-                                <ul class="articles">
-                                    <li class="article-entry">
-                                        <h4> <a href='<?php echo "/post/$q_slug"; ?>' class="d-block text-gray-dark"><?php echo $q_title; ?></a></h4>
-                                        <span class="article-meta"><?php echo $newDate; ?> <a href='<?php echo "/author/$username"; ?>'><?php echo $user['username']; ?></a></span>
-                                        <span class="like-count"><?php echo $q_like; ?></span>
-                                    </li>
-                                </ul>
+                                <div class="forum-item">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <div class="forum-icon"> <i class="fa fa-bolt"></i></div> <a href="<?php echo "/post/$q_slug"; ?>" class="forum-item-title"><?php echo $q_title; ?></a>
+                                            <div class="forum-sub-title"><a href="<?php echo "/author/$username"; ?>"><?php echo $username; ?></a> posted a post.</div>
+                                        </div>
+                                        <div class="col-md-1 forum-info"> <span class="views-number"> <?php echo $q_like ?> </span>
+                                            <div> <small>Likes</small></div>
+                                        </div>
+                                        <div class="col-md-1 forum-info"> <span class="views-number"> <?php echo $q_dislike ?> </span>
+                                            <div> <small>Dislikes</small></div>
+                                        </div>
+                                        <div class="col-md-1 forum-info"> <span class="views-number"> <?php echo $q_like - $q_dislike ?> </span>
+                                            <div> <small>Score</small></div>
+                                        </div>
+                                    </div>
+                                </div>
                                 <?php endwhile; ?>
                         </section>
                     </div>
