705f474305a93c02142cea77cd4a58deda8bf204
Arınç Alp Eren <arinc.alp.98@gmail.com>
2020-02-23 15:55:45 +0300

    Revert "debug"
    This reverts commit f2c5b2d38d7d76c9d9eb7d8f6e975ca124a5207c.

--- admin/header.php
+++ admin/header.php
@@ -4,9 +4,6 @@
     ini_set('display_errors', 1);
     error_reporting(-1); 
 
-    echo "asdaasdasasdasd";
-    exit(0);
-    
     include '../inc/Conn.php';
     $conne = new Mysql();
     $conn = $conne->dbConnect();
