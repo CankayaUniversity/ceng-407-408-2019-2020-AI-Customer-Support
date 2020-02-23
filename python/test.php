32c119d9d2f09d2b1bc3932b57ee2afade8bcb80
atakde <mehata1997@hotmail.com>
2020-02-23 20:45:41 +0300

    python :: fix my error

--- python/test.php
+++ python/test.php
@@ -5,7 +5,7 @@ $question = "What is the difference between the City Navigato";
 if($_SERVER['HTTP_HOST'] == 'localhost'){
     $tmp = exec("C:/Users/mehat/AppData/Local/Programs/Python/Python38-32/python.exe test.py $question");
 } else {
-    $tmp = exec("/usr/bin/python3 test.py $var1 $var2 $var3");
+    $tmp = exec("/usr/bin/python3 test.py $question");
 }
 
 echo $tmp;
