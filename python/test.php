ad876a091521271235d9d3ed9d0b6aa58318edd0
Arınç Alp Eren <arinc.alp.98@gmail.com>
2020-02-24 01:19:28 +0300

    test :: localhost added

--- python/test.php
+++ python/test.php
@@ -4,6 +4,8 @@ $question = "What is the difference between the City Navigato";
 
 if($_SERVER['HTTP_HOST'] == 'localhost'){
     $tmp = exec("C:/Users/mehat/AppData/Local/Programs/Python/Python38-32/python.exe analize.py $question", $out);
+} else if($_SERVER['HTTP_HOST'] == 'localhost:8080'){
+    $tmp = exec("C:/Users/Arinc/AppData/Local/Programs/Python/Python37-32/python.exe analize.py $question", $out);
 } else {
     $tmp = exec("/usr/bin/python3 analize.py $question");
 }
