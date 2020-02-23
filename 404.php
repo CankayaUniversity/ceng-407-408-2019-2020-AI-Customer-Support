d48f7cc03d59f90bf13243f79412e1a3e9a88ff5
atakde <mehata1997@hotmail.com>
2020-02-23 14:34:57 +0300

    404 :: Created 404 page

---
+++ 404.php
@@ -0,0 +1,28 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<div class="page-wrap d-flex flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+                <span class="display-1 d-block">404</span>
+                <div class="mb-4 lead">Oops! We can't seem to find the page you are looking for.</div>
+                <a href="/index.php" class="btn btn-link">Back to Home</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style type="text/css">
+    body {
+        background: #eee;
+    }
+    .container{
+        padding:5%;
+    }
+    .lead{
+        background:#eee;
+        padding:4%;
+    }
+</style>
