ee3ea6d4523bebf3ee387e6174e2717367e27633
atakde <mehata1997@hotmail.com>
2020-02-23 20:35:55 +0300

    Revert "Python :: run script in live server"
    This reverts commit bfacb2eea8bc283a5bb7eee376f9177d7889445c.

--- python/test.py
+++ python/test.py
@@ -1,27 +1,24 @@
-#!/usr/bin/env python
+#import sys
+import json
+#print (sys.argv[1], sys.argv[2], sys.argv[3]), # get parameters
 
-import sys
-# import json
-test = "atakan"
-print (test, sys.argv[2], sys.argv[3]) # get parameters
+import pandas as pd
+import gzip
 
-# import pandas as pd
-# import gzip
+def parse(path):
+  g = gzip.open(path, 'rb')
+  for l in g:
+    yield eval(l)
 
-# def parse(path):
-#   g = gzip.open(path, 'rb')
-#   for l in g:
-#     yield eval(l)
+def getDF(path):
+  i = 0
+  df = {}
+  for d in parse(path):
+    df[i] = d
+    i += 1
+  return pd.DataFrame.from_dict(df, orient='index')
 
-# def getDF(path):
-#   i = 0
-#   df = {}
-#   for d in parse(path):
-#     df[i] = d
-#     i += 1
-#   return pd.DataFrame.from_dict(df, orient='index')
-
-# df = getDF('python/qa_Software.json.gz')
-# df.drop(["answerTime","unixTime"],axis=1,inplace=True)
-# df.to_csv("data.csv")
+df = getDF('python/qa_Software.json.gz')
+df.drop(["answerTime","unixTime"],axis=1,inplace=True)
+df.to_csv("data.csv")
 
