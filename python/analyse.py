88d4d188a666c4d8f20ca0b142ae81023ed87f1e
atakde <mehata1997@hotmail.com>
2020-02-24 20:36:07 +0300

    python :: updated method

--- python/analyse.py
+++ python/analyse.py
@@ -32,10 +32,11 @@ text = ''
 for word in sys.argv[1:]:
     text += word + ' '
 
-update_df = pd.DataFrame(columns=['index' ,'text', 'similarity'])
+update_df = pd.DataFrame(columns=['index' ,'text', 'similarity', 'answer'])
+update_df["answer"] = data.answer.values
+update_df["text"] = data.question.values
 
 indexes = []
-texts = []
 similarities = []
 
 index = -1
@@ -43,13 +44,10 @@ for i in data.question.values:
   index += 1
   sim = cosine_sim(i ,text )
   indexes.append(index)
-  texts.append(i)
   similarities.append(sim)
 
-
 update_df['index'] = indexes
-update_df['text'] = texts
 update_df['similarity'] = similarities
 
 update_df.sort_values(by = 'similarity',ascending=False , inplace=True)
-print(update_df.head())
+print(update_df.iloc[0,3])
