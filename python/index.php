7f4b70ccdef918bf873b4ca1b9ab7deab43716fd
atakde <mehata1997@hotmail.com>
2020-02-25 22:53:37 +0300

    Revert "python :: testing"
    This reverts commit 1ab9c58428ef0a48e206e60f66662b0b01116269.

--- python/index.php
+++ python/index.php
@@ -1,16 +1,18 @@
 <?php
 
-include_once("../inc/Conn.php");
+include_once("inc/Conn.php");
 
 $conne = new Mysql();
 $conn = $conne->dbConnect(); 
 
-if( $_GET["question"] !== NULL && !empty($_GET["question"]) && $_GET["questionID"] !== NULL && !empty($_GET["questionID"]) ) {
-    $question = $_GET["question"];
-    $id = $_GET["questionID"];
+if( isset($_POST["question"] && !empty($_POST["question"]) && isset($_POST["questionID"] && !empty($_POST["questionID"]) ) {
+    
+    $question = $_POST["question"];
+    $id = $_POST["questionID"];
     $runPython =exec('python3 /var/www/html/python/analyse.py ' . $question . " 2>&1");
-    if( $runPython !== NULL && !empty($runPython) ) {
-        $sql = "INSERT INTO comments (c_post_id, c_description, c_author) VALUES ('".$id."', '".$runPython."', 4)";
+
+    if( $runPython !== NULL && isset($runPython) && !empty($runPython) ) {
+        $sql = "INSERT INTO comments (c_post_id, c_description) VALUES ('".$id."', '".$runPython."')";
         $gonder = $conn->prepare($sql);
         $gonder->execute();
     }
