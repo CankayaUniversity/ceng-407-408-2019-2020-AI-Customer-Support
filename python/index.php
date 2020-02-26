5d4e4ef50ad2e06042b815f0226b802d92c1c4f5
atakde <mehata1997@hotmail.com>
2020-02-27 00:16:46 +0300

    python :: change user id

--- python/index.php
+++ python/index.php
@@ -11,7 +11,7 @@ if( $_GET["question"] !== NULL && !empty($_GET["question"]) && $_GET["questionID
     $runPython =exec('python3 /var/www/html/python/analyse.py ' . $question . " 2>&1");
     if( $runPython !== NULL && !empty($runPython) ) {
         $runPython = htmlspecialchars($runPython,ENT_QUOTES);
-        $sql = "INSERT INTO comments (c_post_id, c_description, c_author) VALUES ('".$id."', '".$runPython."', 4)";
+        $sql = "INSERT INTO comments (c_post_id, c_description, c_author) VALUES ('".$id."', '".$runPython."', 12)";
         $gonder = $conn->prepare($sql);
         $gonder->execute();
     }
