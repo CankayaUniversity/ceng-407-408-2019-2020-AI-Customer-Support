ee6d1cbd33b98cdc11e591b7345cfef5df412c35
atakde <mehata1997@hotmail.com>
2020-02-26 20:30:55 +0300

    Python :: fix url encode bug

--- python/index.php
+++ python/index.php
@@ -6,10 +6,11 @@ $conne = new Mysql();
 $conn = $conne->dbConnect(); 
 
 if( $_GET["question"] !== NULL && !empty($_GET["question"]) && $_GET["questionID"] !== NULL && !empty($_GET["questionID"]) ) {
-    $question = $_GET["question"];
+    $question = urldecode($_GET["question"]);
     $id = $_GET["questionID"];
     $runPython =exec('python3 /var/www/html/python/analyse.py ' . $question . " 2>&1");
     if( $runPython !== NULL && !empty($runPython) ) {
+        $runPython = htmlspecialchars($runPython,ENT_QUOTES);
         $sql = "INSERT INTO comments (c_post_id, c_description, c_author) VALUES ('".$id."', '".$runPython."', 4)";
         $gonder = $conn->prepare($sql);
         $gonder->execute();
