5ff9079279dc0f914c397450af080ebf9100ee68
Arınç Alp Eren <arinc.alp.98@gmail.com>
2020-02-28 14:27:41 +0300

    search :: search bar updated

--- search.php
+++ search.php
@@ -4,13 +4,19 @@ include 'helpers/homeController.php';
         $conne = new Mysql();
         $conn = $conne->dbConnect();
         $term = $_REQUEST['term']; 
-        $query = $conn->query("SELECT * FROM questions WHERE q_title LIKE '%".$term."%'",PDO::FETCH_ASSOC);
+        $query = $conn->query("SELECT * FROM questions WHERE q_title LIKE '%".$term."%' LIMIT 4;",PDO::FETCH_ASSOC);
+        $rowCount = $conne->selectRowCount("SELECT * FROM questions WHERE q_title LIKE '%".$term."%';");
         $result=$query->fetchAll();
-        if ($result) { 
+        if ($result && $rowCount > 5) { 
             foreach ($result as $key => $value) {
                 echo '<a href="/post/'.$value["slug"].'"><li class="list-group-item">'.$value["q_title"].'</li></a>';
             }
-        }else{
+            echo '<a href="/tag/'.$term.'"><li class="list-group-item">Click here for more related questions!</li></a>';
+        } else if ($result && $rowCount <= 5 && $rowCount > 0){
+            foreach ($result as $key => $value) {
+                echo '<a href="/post/'.$value["slug"].'"><li class="list-group-item">'.$value["q_title"].'</li></a>';
+            }
+        } else {
             echo '<li class="list-group-item">No matching records were found.</li>';
         }
     }
