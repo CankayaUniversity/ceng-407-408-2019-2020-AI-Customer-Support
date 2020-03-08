d0db6c92bc360ba693fac26759d67146e090123b
Arınç Alp Eren <arinc.alp.98@gmail.com>
2020-03-08 19:07:48 +0300

    data,index :: chart bug fixed

--- admin/data.php
+++ admin/data.php
@@ -6,9 +6,10 @@ include '../inc/Conn.php';
 $conne = new Mysql();
 $conn = $conne->dbConnect();
 
-$getAllQuestions = $conne->selectFreeRun("SELECT COUNT(CASE WHEN is_solved= -1 THEN 1 END) AS notsolved, COUNT(CASE WHEN is_solved= 1 THEN 1 END) AS solved FROM questions");
+$getAllQuestions = $conne->selectFreeRun("SELECT COUNT(CASE WHEN is_solved= -1 THEN 1 END) AS notanswered, COUNT(CASE WHEN is_solved= 1 THEN 1 END) AS solved, COUNT(CASE WHEN is_solved=0 THEN 1 END) AS notsolved FROM questions");
 
 $data["solved"] = $getAllQuestions[0]["solved"];
 $data["notsolved"] = $getAllQuestions[0]["notsolved"];
+$data["notanswered"] = $getAllQuestions[0]["notanswered"];
 
 echo json_encode($data);
--- admin/index.php
+++ admin/index.php
@@ -1,37 +1,35 @@
 <?php
 include "header.php";
 ?>
-  <body>
+<body>
     <div id="piechart_3d" style="width: 900px; height: 500px;"></div>
-  </body>
-  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-  <script>
-  google.charts.load("current", {
-      packages: ["corechart"]
-  });
-  google.charts.setOnLoadCallback(drawChart);
+</body>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script>
+google.charts.load("current", {
+    packages: ["corechart"]
+});
+google.charts.setOnLoadCallback(drawChart);
 
-  function drawChart() {
-      {
-          $.post("data.php",
-              function(response) {
-
-                  console.log(typeof(response.solved));
-                  var data = google.visualization.arrayToDataTable([
-                      ['Questions', ''],
-                      ['solved', parseInt(response.solved)],
-                      ['notsolved', parseInt(response.solved)]
-                  ]);
-                  var options = {
-                      title: 'Questions',
-                      is3D: true,
-                  };
-
-                  var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-                  chart.draw(data, options);
-
-              });
-      }
-  }
-  </script>
+function drawChart() {
+    {
+        $.post("data.php",
+            function(response) {
+                console.log(typeof(response.solved));
+                var data = google.visualization.arrayToDataTable([
+                    ['Questions', ''],
+                    ['solved', parseInt(response.solved)],
+                    ['notsolved', parseInt(response.notsolved)],
+                    ['notanswered', parseInt(response.notanswered)]
+                ]);
+                var options = {
+                    title: 'Questions',
+                    is3D: true,
+                };
+                var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+                chart.draw(data, options);
+            });
+    }
+}
+</script>
 
