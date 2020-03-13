b10b7c9a53d609ea1852ef96d6d71b058f95a4c7
Arınç Alp Eren <40849529+alpekin98@users.noreply.github.com>
2020-03-13 20:21:02 +0300

    Profile :: Errors now alert

--- profile.php
+++ profile.php
@@ -75,6 +75,7 @@ if ($count = $query->rowCount()) {
 
 if (isset($_POST["submit"])) {
     $target_dir = "uploads/";
+    $imagesize = 5000000;
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
@@ -91,23 +92,25 @@ if (isset($_POST["submit"])) {
     }
     // Check if file already exists
     if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
+        echo "<script>alert('Sorry, file already exists.');</script>";
+
         $uploadOk = 0;
     }
     // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
+    if ($_FILES["fileToUpload"]["size"] > $imagesize) {
+        echo "<script>alert('Sorry, your file is too large. Maximum ".($imagesize/1000000)." MB is allowed');</script>";
         $uploadOk = 0;
     }
     // Allow certain file formats
     if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
         && $imageFileType != "gif") {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        echo "<script>alert('Sorry, only JPG, JPEG, PNG & GIF files are allowed.');</script>";
         $uploadOk = 0;
     }
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
+        echo "<script>alert('Sorry, your file was not uploaded.');</script>";
+        
         // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
