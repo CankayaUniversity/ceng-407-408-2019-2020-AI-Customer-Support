fcb822e2c1efadd317ab4a5f34add6ba86ea1084
atakde <mehata1997@hotmail.com>
2020-03-22 17:17:24 +0300

    Scripts :: testing mail

---
+++ scripts/testMail.php
@@ -0,0 +1,17 @@
+<?php
+
+$headers =  'MIME-Version: 1.0' . "\r\n"; 
+$headers .= 'From: Your name <info@address.com>' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("cpukarsilastir@gmail.com","My subject",$msg, $headers);
+
+?>
+
