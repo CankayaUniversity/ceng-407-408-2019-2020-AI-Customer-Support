9d47eb2bc281215c266c1dc297b00b637333fd09
atakde <mehata1997@hotmail.com>
2020-03-23 17:48:38 +0300

    helpers & scripts :: added test mail, created mailConfig

---
+++ helpers/classes/mailConfig.php
@@ -0,0 +1,10 @@
+<?php
+
+Class mailConfig {
+
+    const SMTP_HOST = 'smtp.gmail.com';
+    const SMTP_PORT = '587';
+    const SMTP_USER = 'testatakde@gmail.com';
+    const SMTP_PASSWORD = '';
+
+}
--- scripts/testMail.php
+++ scripts/testMail.php
@@ -1,17 +1,44 @@
 <?php
 
-$headers =  'MIME-Version: 1.0' . "\r\n"; 
-$headers .= 'From: Your name <info@address.com>' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+require '.././vendor/autoload.php';
 
-// the message
-$msg = "First line of text\nSecond line of text";
+$mail = new PHPMailer\PHPMailer\PHPMailer;
 
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
+$mail->isSMTP();
 
-// send email
-mail("cpukarsilastir@gmail.com","My subject",$msg, $headers);
 
-?>
+//$mail->SMTPDebug = PHPMailer\PHPMailer\SMTP::DEBUG_SERVER;
 
+$mail->Host = mailConfig::SMTP_HOST;
+
+$mail->SMTPOptions = array(
+'ssl' => array(
+'verify_peer' => false,
+'verify_peer_name' => false,
+'allow_self_signed' => true
+)
+);
+
+$mail->Port = mailConfig::SMTP_PORT;
+$mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;
+$mail->SMTPAuth = true;
+$mail->Username = mailConfig::SMTP_USER;
+$mail->Password = mailConfig::SMTP_PASSWORD;
+
+$mail->setFrom('from@example.com', 'First Last');
+$mail->addReplyTo('replyto@example.com', 'First Last');
+$mail->addAddress('cpukarsilastir@gmail.com', 'John Doe');
+$mail->Subject = 'PHPMailer TESTTEST';
+$mail->Body = 'Hello, this is my message.';
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo 'Mailer Error: '. $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
+    //Section 2: IMAP
+    //Uncomment these to save your message in the 'Sent Mail' folder.
+    #if (save_mail($mail)) {
+    #    echo "Message saved!";
+    #}
+}
