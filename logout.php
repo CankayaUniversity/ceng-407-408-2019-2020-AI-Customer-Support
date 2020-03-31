f1a02e0a819d521764bca10036997372e6ecd6d1
aydincavid <muhcavid@gmail.com>
2020-03-31 14:55:34 +0300

    logout.php :: last login

--- logout.php
+++ logout.php
@@ -1,6 +1,17 @@
 <?php 
+include 'helpers/SEOHelper.php';
+include 'helpers/Functions.php';
 
 session_start();
+$id = $_SESSION["user_UserID"];
+$conne = new Mysql();
+$conn = $conne->dbConnect();
+
+    $query = $conn->prepare("UPDATE users SET last_login=CURRENT_TIMESTAMP WHERE user_id=:id");
+    $query->execute([
+        ':id' =>  $id
+    ]);
+  
 session_unset();
 session_destroy();
 
