52d465654e77f6809735df099f827ccbd876be41
Arınç Alp Eren <arinc.alp.98@gmail.com>
2020-04-20 22:25:11 +0300

    new_answer :: now right answers are stored in csv

---
+++ python/new_answer.py
@@ -0,0 +1,15 @@
+question = ''
+new_answer = ''
+flag = 1
+for word in sys.argv[1:]:
+    if(word == "/"):
+        flag=0
+        continue
+    if(flag == 1):
+        new_answer += word + ' '
+    elif(flag == 0):
+        question += word + ' '
+
+data = pd.read_csv("data.csv")
+
+data[data.question == question].answer = new_answer
