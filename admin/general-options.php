2211679161558f304ee4259ac7aaa4429a233d3b
atakde <mehata1997@hotmail.com>
2020-04-29 17:46:06 +0300

    admin/general-options :: updated

--- admin/general-options.php
+++ admin/general-options.php
@@ -1,55 +1,59 @@
-<?php
-include "header.php";
+<?php include_once("header.php");
+$siteSettings = $conne->selectAll("site_settings");
 ?>
 <body>
     <div class="container">
         <form method="post">
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default" name="SiteTitle">Site title</span>
+                    <span class="input-group-text" id="inputGroup-sizing-default">Site title</span>
                 </div>
-                <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                <input type="text" class="form-control" placeholder="<?php echo $siteSettings[0]['site_title'];?>"  aria-label="Default" aria-describedby="inputGroup-sizing-default" name="SiteTitle">
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default" >Slogan</span>
                 </div>
-                <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" name="Slogan">
+                <input type="text" class="form-control" placeholder="<?php echo $siteSettings[0]['slogan'];?>" aria-label="Default" aria-describedby="inputGroup-sizing-default" name="Slogan">
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default" >System address (URL)</span>
                 </div>
-                <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" name="SystemAdress">
+                <input type="text" class="form-control" placeholder="<?php echo $siteSettings[0]['system_address'];?>" aria-label="Default" aria-describedby="inputGroup-sizing-default" name="SystemAdress">
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default" >Site address (URL)</span>
                 </div>
-                <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" name="SiteAddress">
+                <input type="text" class="form-control" placeholder="<?php echo $siteSettings[0]['site_address'];?>" aria-label="Default" aria-describedby="inputGroup-sizing-default" name="SiteAddress">
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default" >E-mail address</span>
                 </div>
-                <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" name="EmailAddress">
+                <input type="email" class="form-control" placeholder="<?php echo $siteSettings[0]['mail_address'];?>" aria-label="Default" aria-describedby="inputGroup-sizing-default" name="EmailAddress">
             </div>
-            <button type="submit" class="btn btn-outline-primary">Send</button>
+            <button type="submit" name="submit" class="btn btn-success">Send</button>
         </form>
     </div>
 </body>
 
 
 <?php
-if (isset($_POST['Slogan'])) {
+if (isset($_REQUEST['submit'])) {
+    $SiteTitle = $_POST['SiteTitle'];
     $Slogan = $_POST['Slogan'];
     $SystemAdress = $_POST['SystemAdress'];
     $SiteAddress = $_POST['SiteAddress'];
     $EmailAddress = $_POST['EmailAddress'];
     $conne->freeRun("TRUNCATE TABLE site_settings");
-    $sql = "INSERT INTO site_settings(slogan, system_address, site_address, mail_address) VALUES
-            ('$Slogan','$SystemAdress','$SiteAddress','$EmailAddress')";
+    $sql = "INSERT INTO site_settings(slogan, system_address, site_address, mail_address, site_title) VALUES
+            ('$Slogan','$SystemAdress','$SiteAddress','$EmailAddress', '$SiteTitle')";
     $conne->freeRun($sql);
+    
+    echo "<meta http-equiv='refresh' content='0'>";
+
 }
 
 ?>
